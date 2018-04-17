bf0ed19a0726574e94d30f9fea9fa3c71808c387
Gadelve <gadelvejob@gmail.com>
2018-04-17 13:06:20 +0200

    Mi primer commit

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -6,7 +6,25 @@ package com.teaching.android;
 public class JavaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+        byte a = 2;
+        char b = 'G';
+        short c = 65;
+        int d = 100;
+        long e = 1899L;
+        float f = 18.99f;
+        double g = 77.98;
+        boolean bol= true;
+
+            System.out.println("Mi primer programa Java");
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(d);
+            System.out.println(e);
+            System.out.println(f);
+            System.out.println(g);
+            System.out.println(bol);
+
     }
 
 }
